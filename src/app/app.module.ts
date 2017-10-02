46244fd972dec430302356c1d2830001c02588a2
Thornton <CThornton@pmi.corp>
2017-10-02 11:22:06 -0400

    Initial commit for Angular2 Heroes Editor

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { RouterModule }  from '@angular/router';
+
+import { AppComponent }  from './app.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }  from './heroes.component';
+import { HeroService } from './hero.service';
+
+RouterModule.forRoot([
+  {
+    path: 'heroes';
+    component: HeroesComponent
+  }
+])
+
+@NgModule({
+  imports:      [ 
+    BrowserModule,
+    FormsModule, 
+    RouterModule.forRoot([
+      {
+        path: 'heroes';
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ])],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+  providers: [HeroService],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
